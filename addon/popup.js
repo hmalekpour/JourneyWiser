c0d94cc2ed173bca422723b46b5fc64864e9e5bc
Ubuntu <ubuntu@ip-10-0-0-6.us-east-2.compute.internal>
2020-02-15 05:08:41 +0000

    DB IP-address updated

--- addon/popup.js
+++ addon/popup.js
@@ -12,7 +12,7 @@ function myFunction() {
         var path_str = url.pathname;
         var listing_id = path_str.substring(path_str.lastIndexOf("/") + 1);
         var listing_date = date_picker.value;
-        fetch(`http://18.188.79.176/check-listing?id=${listing_id}&date=${listing_date}`).then(r => r.text()).then(result => {
+        fetch(`http://18.221.216.108/check-listing?id=${listing_id}&date=${listing_date}`).then(r => r.text()).then(result => {
           // Result now contains the response text, do what you want...
           alert(result)
         })
