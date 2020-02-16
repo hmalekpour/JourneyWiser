ce1e16234505e905a276ede672910a8f0ff75dba
Ubuntu <ubuntu@ip-10-0-0-6.us-east-2.compute.internal>
2020-02-16 08:51:52 +0000

    addon improved

--- addon/popup.js
+++ addon/popup.js
@@ -1,5 +1,5 @@
 
-function myFunction() {
+function show_results(id1, id2) {
   chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
     var url_string = tabs[0].url;
 
@@ -13,15 +13,35 @@ function myFunction() {
         var listing_id = path_str.substring(path_str.lastIndexOf("/") + 1);
         var listing_date = date_picker.value;
         fetch(`http://18.221.216.108/check-listing?id=${listing_id}&date=${listing_date}`).then(r => r.text()).then(result => {
-          // Result now contains the response text, do what you want...
-          alert(result)
-        })
+          //display results
+          if (result)
+          {
+            var res = result.split(",");
+            if (res[0] == '999')
+            {
+              document.getElementById(id1).innerHTML = "Booking closed for selected date.";
+              document.getElementById(id2).innerHTML = 'Average monthly lead time is ' + res[1] + ' days.';
+            }
+            else
+            {
+              document.getElementById(id1).innerHTML = 'Lead time for selected date is ' + res[0] + ' days.';
+              document.getElementById(id2).innerHTML = 'Average monthly lead time is ' + res[1] + ' days.';
+            }
+          }
+          else
+          {
+            document.getElementById(id1).innerHTML = "No historical data is available for this listing.";
+          }
+          });
       }
     }
   });
-} 
+}
+
 
 document.addEventListener('DOMContentLoaded', function () {
   var button = document.getElementById('checkPage');
-  button.addEventListener('click', myFunction);
+  button.addEventListener('click', function()
+    {show_results('lead-time', 'month-avg');}
+  );
 });
